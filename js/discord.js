7d97cd7c97426cb3f674ec319017f6e812b5461a
kateo <cartercrouch12@gmail.com>
2021-10-23 19:19:03 -0700

    main update 1

--- js/discord.js
+++ js/discord.js
@@ -1,4 +1,13 @@
-document.getElementById("button").onclick = function(){
 
-    document.getElementById("discord").innerHTML = "Joining Discord!"
+
+
+
+document.getElementById("button").onclick = function(){
+    document.getElementById("discord").innerHTML = "Joining Discord!" 
+    // makes button disapear, and txt change
+    document.getElementById("button").style.display = "none";
+    
+    
+    
+    console.log("dev_work ip");
 }
